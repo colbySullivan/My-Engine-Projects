25b646d78f08debefe51d3614d5ac82745d1cca6
(C35) Colby Sullivan <cwsullivan@smu.edu>
2025-09-09 23:07:10 -0600

    Added GameCommon.cpp Debug ring created but not connected
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 424908]

---
+++ Starship/Code/Game/GameCommon.cpp
@@ -0,0 +1,60 @@
+#include "GameCommon.hpp"
+#include "Engine/Math/Vec3.hpp"
+#include "Engine/Math/Vec2.hpp"
+#include <Engine/Core/Vertex.hpp>
+#include "Engine/Core/Engine.hpp"
+#include "Engine/Renderer/Renderer.hpp"  
+#include "App.hpp";
+
+void GameCommon::DebugDrawRing(Vec2 const& center, float radius, float thickness, Rgba8 const& color)
+{
+	float halfThickness = 0.5f * thickness;
+	float innerRadius = radius - halfThickness;
+	float outerRadius = radius + halfThickness;
+	constexpr int NUM_SIDES = 32;
+	constexpr int NUM_TRIS = 2 * NUM_SIDES;
+	constexpr int NUM_VERTS = 3 * NUM_TRIS;
+	Vertex verts[NUM_VERTS];
+	constexpr float DEGREES_PER_SIDE = 360.f / static_cast<float>( NUM_SIDES );
+	for (int sideNum = 0; sideNum < NUM_SIDES; ++sideNum)
+	{
+		// Compute angle-related terms
+		float startDegrees = DEGREES_PER_SIDE * static_cast<float>(sideNum);
+		float endDegrees = DEGREES_PER_SIDE * static_cast<float>(sideNum + 1);
+		float cosStart = CosDegrees(startDegrees);
+		float sinStart = SinDegrees(startDegrees);
+		float cosEnd = CosDegrees(endDegrees);
+		float sinEnd = SinDegrees(endDegrees);
+
+		// Compute inner and outer positions
+		Vec3 innerStartPos = Vec3(center.x + innerRadius * cosStart, center.y + innerRadius * sinStart, 0.0f);
+		Vec3 outerStartPos = Vec3(center.x + outerRadius * cosStart, center.y + outerRadius * sinStart, 0.0f);
+		Vec3 outerEndPos = Vec3(center.x + outerRadius * cosEnd, center.y + outerRadius * sinEnd, 0.0f);
+		Vec3 innerEndPos = Vec3(center.x + innerRadius * cosEnd, center.y + innerRadius * sinEnd, 0.0f);
+
+		// Trapezoid is made of two triangles: ABC and DEF
+		// A is inner end; B is inner Start; C is outer Start
+		// D is inner end; E is outer Start; F is outer end
+		int vertIndexA = (6 * sideNum) + 0;
+		int vertIndexB = (6 * sideNum) + 1;
+		int vertIndexC = (6 * sideNum) + 2;
+		int vertIndexD = (6 * sideNum) + 3;
+		int vertIndexE = (6 * sideNum) + 4;
+		int vertIndexF = (6 * sideNum) + 5;
+
+		verts[vertIndexA].m_position = innerEndPos;
+		verts[vertIndexB].m_position = innerStartPos;
+		verts[vertIndexC].m_position = outerStartPos;
+		verts[vertIndexA].m_color = color;
+		verts[vertIndexB].m_color = color;
+		verts[vertIndexC].m_color = color;
+
+		verts[vertIndexD].m_position = innerEndPos;
+		verts[vertIndexE].m_position = outerStartPos;
+		verts[vertIndexF].m_position = outerEndPos;
+		verts[vertIndexD].m_color = color;
+		verts[vertIndexE].m_color = color;
+		verts[vertIndexF].m_color = color;
+	}
+	g_engine->m_render->DrawVertexArray(NUM_VERTS, verts);
+}
--- Starship/Code/Game/GameCommon.hpp
+++ Starship/Code/Game/GameCommon.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <Engine/Math/MathUtils.hpp>
+#include <Engine/Core/Rgba8.hpp>
 
 constexpr int NUM_STARTING_ASTEROIDS = 6;
 constexpr int MAX_ASTEROIDS = 12;
@@ -18,3 +20,7 @@ constexpr float PLAYER_SHIP_ACCELERATION = 30.f;
 constexpr float PLAYER_SHIP_TURN_SPEED = 300.f;
 constexpr float PLAYER_SHIP_PHYSICS_RADIUS = 1.75f;
 constexpr float PLAYER_SHIP_COSMETIC_RADIUS = 2.25f;
+class GameCommon
+{
+	void DebugDrawRing(Vec2 const& center, float radius, float thickness, Rgba8 const& color);
+};
