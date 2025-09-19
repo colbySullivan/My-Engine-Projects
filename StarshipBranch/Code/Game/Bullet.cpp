4d16e034292c8b027fc14cd55708aadb8f1b59c7
(C35) Colby Sullivan <cwsullivan@smu.edu>
2025-09-19 09:35:16 -0600

    Branching
    //depot/C35/Students/cwsullivan/SD/Starship/...
    
    to //depot/C35/Students/cwsullivan/SD/StarshipBranch/...
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 426838]

---
+++ StarshipBranch/Code/Game/Bullet.cpp
@@ -0,0 +1,65 @@
+#include "Bullet.hpp"
+#include <Engine/Core/VertexUtils.hpp>
+#include "Engine/Core/Engine.hpp"
+#include "Engine/Renderer/Renderer.hpp"
+
+
+Bullet::Bullet(Game* owner, Vec2 const& startPos)
+	: Entity( owner, startPos)
+{
+	m_physicsRadius = BULLET_PHYSICS_RADIUS;
+	m_cosmeticRadius = BULLET_COSMETIC_RADIUS;
+	m_velocity = GetForwardNormal() * BULLET_SPEED;
+	InitializeLocalVerts();
+}
+
+Bullet::~Bullet()
+{
+
+}
+
+void Bullet::Update(float deltaSeconds)
+{
+	m_position += m_velocity * deltaSeconds;
+
+	if (IsOffscreen())
+	{
+		m_isDead = true;
+		m_isGarbage = true;
+	}
+}
+
+void Bullet::Render() const
+{
+	if (m_isDead)
+		return;
+
+	Vertex tempWorldVerts[NUM_BULLET_VERTS];
+	for (int vertIndex = 0; vertIndex < NUM_BULLET_VERTS; ++vertIndex)
+	{
+		tempWorldVerts[vertIndex] = m_localVerts[vertIndex];
+	}
+	TransformVertexArrayXY3D(NUM_BULLET_VERTS, tempWorldVerts, 1.f, m_orientationDegrees, m_position);
+	g_engine->m_render->DrawVertexArray(NUM_BULLET_VERTS, tempWorldVerts);
+
+	if (g_theApp->g_drawDebug)
+		DebugRender();
+}
+
+void Bullet::InitializeLocalVerts()
+{
+	m_localVerts[0].m_position = Vec3(0.5f, 0.0f, 0.0f);
+	m_localVerts[1].m_position = Vec3(0.0f, 0.5f, 0.0f);
+	m_localVerts[2].m_position = Vec3(0.0f, -0.5f, 0.0f);
+	m_localVerts[0].m_color = Rgba8(255, 255, 255, 255);
+	m_localVerts[1].m_color = Rgba8(255, 255, 0, 255);
+	m_localVerts[2].m_color = Rgba8(255, 255, 0, 255);
+
+	m_localVerts[3].m_position = Vec3(0.0f, -0.5f, 0.0f);
+	m_localVerts[4].m_position = Vec3(0.0f, 0.5f, 0.0f);
+	m_localVerts[5].m_position = Vec3(-2.0f, 0.0f, 0.0f);
+	m_localVerts[3].m_color = Rgba8(255, 0, 0, 255);
+	m_localVerts[4].m_color = Rgba8(255, 0, 0, 255);
+	m_localVerts[5].m_color = Rgba8(255, 0, 0, 0);
+}
+
---
+++ StarshipBranch/Code/Game/Game.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include "App.hpp"
+#include "GameCommon.hpp"
+
+//------------------------------------------------------------------------------
+
+class App;
+class PlayerShip;
+class Asteroid;
+class Bullet;
+class Entity;
+
+
+//------------------------------------------------------------------------------
+
+class Game
+{
+public:
+	Game(App* owner);
+	~Game();
+	void Startup();
+	void Update(float deltaSeconds);
+	void Render() const;
+	void Shutdown();
+
+	
+	Asteroid* SpawnRandomAsteroid();
+	Bullet* SpawnBullet(Vec2 const& pos, float forwardDegrees);
+	PlayerShip*		m_playerShip = nullptr;
+
+private:
+	void UpdateEntities(float deltaSeconds);
+	void CheckBulletsVsAsteroids();
+	void CheckBulletsVsAsteroid(Bullet& bullet, Asteroid& asteroid);
+	void CheckAsteroidsVsShips();
+	void CheckAsteroidVsShip(Asteroid& asteroid, PlayerShip& ship);
+	void RenderEntities() const;
+	void DestroyGarbageEntities();
+
+	App*			m_app;
+	Bullet*			m_bullets[MAX_BULLETS] = {};
+	Asteroid*		m_asteroid[MAX_ASTEROIDS] = {};
+	
+
+	//Camera*			m_gameCamera = nullptr;
+};
