9cd66880dc63441774f0f1f0768f3093b784dc7f
(C35) Colby Sullivan <cwsullivan@smu.edu>
2025-09-19 17:32:09 -0600

    Unit tests running for math
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 426918]

--- MathUnitTests/Code/Game/Main.cpp
+++ MathUnitTests/Code/Game/Main.cpp
@@ -17,9 +17,9 @@
 //-----------------------------------------------------------------------------------------------
 void RunTestSets() 
 {
-	RunTests_MP1A01(); //TODO
-	RunTests_MP1A02();	// Uncomment this line after adding the MP1-A2 test code
-	RunTests_MP1A03();	// Uncomment this line after adding the MP1-A3 test code
+	RunTests_MP1A01(); 
+	RunTests_MP1A02();	
+	RunTests_MP1A03();
 	// 	RunTests_MP1A04();	// Uncomment this line after adding the MP1-A4 test code
 	// 	RunTests_MP1A05();	// Uncomment this line after adding the MP1-A5 test code
 	// 	RunTests_MP1A06();	// Uncomment this line after adding the MP1-A6 test code
