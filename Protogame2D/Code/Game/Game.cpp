6b81231a7faa2f0a064e8ec8c086258c1590ed65
(C35) Colby Sullivan <cwsullivan@smu.edu>
2025-10-27 16:00:08 -0600

    Rename protogame to Protogame2D
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 435670]

--- Protogame2D/Code/Game/Game.cpp
+++ Protogame2D/Code/Game/Game.cpp
@@ -266,7 +266,7 @@ void Game::RenderAttractMode() const
 	g_engine->m_render->DrawVertexArray( NUM_BLACK_HOLE_VERTS, tempHoleWorldVerts );
 
 	// Title
-	char title[32] = "Starship Gold";
+	char title[32] = "Protogame2D";
 	for ( int charIndex = 0; charIndex < 18; ++charIndex )
 	{
 		char singleChar[2] = { title[charIndex], '\0' };
