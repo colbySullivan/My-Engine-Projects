aa4273f7d14e2847352ddac4237a09ffcb56c068
(C35) Colby Sullivan <cwsullivan@smu.edu>
2025-11-14 14:20:55 -0600

    Moved RaycastResult2D from Game::map.hpp to engine
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 442184]

--- Libra/Code/Game/Map.hpp
+++ Libra/Code/Game/Map.hpp
@@ -8,19 +8,19 @@
 #include <vector>
 
 //-----------------------------------------------------------------------------------------------
-struct RaycastResult2D
-{
-	// Basic raycast result information (required)
-	bool	m_didImpact = false;
-	float	m_impactDist = 0.f;
-	Vec2	m_impactPos;
-	Vec2	m_impactNormal;
-
-	// Original raycast information (optional)
-	Vec2	m_rayStartPos;
-	Vec2	m_rayFwdNormal;
-	float	m_rayMaxLength = 0.f;
-};
+//struct RaycastResult2D
+//{
+//	// Basic raycast result information (required)
+//	bool	m_didImpact = false;
+//	float	m_impactDist = 0.f;
+//	Vec2	m_impactPos;
+//	Vec2	m_impactNormal;
+//
+//	// Original raycast information (optional)
+//	Vec2	m_rayStartPos;
+//	Vec2	m_rayFwdNormal;
+//	float	m_rayMaxLength = 0.f;
+//};
 
 //-----------------------------------------------------------------------------------------------
 struct MapDef
