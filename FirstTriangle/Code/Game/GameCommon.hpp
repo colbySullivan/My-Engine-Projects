de154465872fc153ed7687ab1e8f2762df8ccf47
(C35) Colby Sullivan <cwsullivan@smu.edu>
2026-01-25 14:08:30 -0600

    Remove more gamecommon values // Rename project
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 448127]

--- FirstTriangle/Code/Game/GameCommon.hpp
+++ FirstTriangle/Code/Game/GameCommon.hpp
@@ -1,15 +1,3 @@
 #pragma once
 #include <Engine/Math/MathUtils.hpp>
 #include <Engine/Core/Rgba8.hpp>
-
-constexpr float WORLD_SIZE_X = 200.f;
-constexpr float WORLD_SIZE_Y = 100.f;
-
-constexpr float WORLD_CENTER_X = WORLD_SIZE_X / 2.f;
-constexpr float WORLD_CENTER_Y = WORLD_SIZE_Y / 2.f;
-
-constexpr float SCREEN_SIZE_X = 1600.f;
-constexpr float SCREEN_SIZE_Y = 800.f;
-
-constexpr float SCREEN_CENTER_X = SCREEN_SIZE_X / 2.f;
-constexpr float SCREEN_CENTER_Y = SCREEN_SIZE_Y / 2.f;
