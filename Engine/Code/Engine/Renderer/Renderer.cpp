38d2c2b17b856a3e441635dab8d2cde5b30cadae
(C35) Colby Sullivan <cwsullivan@smu.edu>
2026-01-29 23:45:04 -0600

    Fixed linker error for FirstTriangle
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 448494]

--- Engine/Code/Engine/Renderer/Renderer.cpp
+++ Engine/Code/Engine/Renderer/Renderer.cpp
@@ -21,7 +21,7 @@
 #pragma comment(lib, "d3dcompiler.lib")
 
 #include "Engine/Core/Engine.hpp"
-#include "Engine/Renderer/Renderer.hpp"  
+#include "Engine/Renderer/Renderer.hpp"
 #include "Engine/Renderer/Camera.hpp"
 #include "Engine/Core/Time.hpp"
 #include "Engine/Input/InputSystem.hpp"
@@ -37,9 +37,6 @@
 #pragma comment(lib, "dxguid.lib")
 #endif
 
-std::vector<uint8_t> m_vertexShaderByteCode;
-std::vector<uint8_t> m_pixelShaderByteCode;
-
 #if defined(ENGINE_DEBUG_RENDER)
 void* m_dxgiDebug = nullptr;
 void* m_dxgiDebugModule = nullptr;
@@ -48,33 +45,6 @@ void* m_dxgiDebugModule = nullptr;
 #pragma comment(lib, "opengl32")
 
 HGLRC g_openGLRenderingContext = nullptr; 
-
-const char* shaderSource = R"(
-	struct vs_input_t
-	{
-		float3 localPosition : POSITION;
-		float4 color : COLOR;
-		float2 uv : TEXCOORD;
-	};
-	struct v2p_t
-	{
-		float4 position : SV_Position;
-		float4 color : COLOR;
-		float2 uv : TEXCOORD;
-	};
-	v2p_t VertexMain(vs_input_t input)
-	{
-		v2p_t v2p;
-		v2p.position = float4(input.localPosition, 1);
-		v2p.color = input.color;
-		v2p.uv = input.uv;
-		return v2p;
-	};
-	float4 PixelMain(v2p_t input) : SV_Target0
-	{
-		return float4(input.color);
-	};
-)";
 
 Renderer::Renderer( RenderConfig const& config )
 	: m_config( config )
