529841cacbdec26fea6390d37b96f168c7988cc3
(C35) Colby Sullivan <cwsullivan@smu.edu>
2026-02-05 14:45:19 -0600

    Image class
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 449449]

--- Engine/Code/Engine/Core/Image.cpp
+++ Engine/Code/Engine/Core/Image.cpp
@@ -1,4 +1,58 @@
 #define STB_IMAGE_IMPLEMENTATION // Exactly one .CPP (this Image.cpp) should #define this before #including stb_image.h
 #include "ThirdParty/stb/stb_image.h"
 #include "Engine/Core/Image.hpp"
+#include "Engine/Core/ErrorWarningAssert.hpp"
+#include "Engine/Core/StringUtils.hpp"
 
+//-----------------------------------------------------------------------------------------------
+Image::Image( char const* imageFilePath )
+	:m_imageFilePath( imageFilePath )
+{
+	int numComponents = 0;
+
+	stbi_set_flip_vertically_on_load( 1 );
+
+	unsigned char* imageDataByes = stbi_load( imageFilePath, &m_dimensions.x, &m_dimensions.y, &numComponents, 0 );
+	GUARANTEE_OR_DIE( imageDataByes != nullptr, Stringf( "Failed to load image %s", imageFilePath ) );
+	GUARANTEE_OR_DIE( numComponents == 3 || numComponents == 4, Stringf( "Can't load image from file %s; has %i components; we only support 3 or 4", imageFilePath, numComponents ) );
+
+	int numTexels = m_dimensions.x * m_dimensions.y;
+	m_texelRgba8Data.resize( numTexels );
+
+	for ( int texelIndex = 0; texelIndex < numTexels; ++texelIndex )
+	{
+		Rgba8& texel = m_texelRgba8Data[texelIndex];
+		texel.r = imageDataByes[( texelIndex * numComponents ) + 0];
+		texel.g = imageDataByes[( texelIndex * numComponents ) + 1];
+		texel.b = imageDataByes[( texelIndex * numComponents ) + 2];
+		texel.a = ( numComponents == 3 ) ? 255 : imageDataByes[( texelIndex * numComponents ) + 3];
+	}
+
+	stbi_image_free( imageDataByes );
+}
+
+//-----------------------------------------------------------------------------------------------
+Image::Image( IntVec2 size, Rgba8 color )
+	: m_dimensions( size )
+{
+	int numTexels = m_dimensions.x * m_dimensions.y;
+	m_texelRgba8Data.resize( numTexels, color );
+}
+
+//-----------------------------------------------------------------------------------------------
+IntVec2 Image::GetDimensions() const
+{
+	return m_dimensions;
+}
+
+//-----------------------------------------------------------------------------------------------
+const std::string& Image::GetImageFilePath() const
+{
+	return m_imageFilePath;
+}
+
+//-----------------------------------------------------------------------------------------------
+const void* Image::GetRawData() const
+{
+	return &m_texelRgba8Data;
+}
--- Engine/Code/Engine/Core/Image.hpp
+++ Engine/Code/Engine/Core/Image.hpp
@@ -1,1 +1,26 @@
 #pragma once
+#include "Engine/Math/IntVec2.hpp"
+#include "Engine/Core/Rgba8.hpp"
+#include <vector>
+#include <string>
+
+class Image
+{
+	friend class Renderer;
+
+public:
+	Image() = default;
+	~Image() = default;
+	Image( const char* imageFilePath );
+	Image( IntVec2 size, Rgba8 color );
+
+	IntVec2 GetDimensions() const;
+	const std::string & GetImageFilePath() const;
+	const void* GetRawData() const;
+
+private:
+	std::string m_imageFilePath;
+	IntVec2 m_dimensions;
+	std::vector<Rgba8> m_texelRgba8Data;
+
+};
