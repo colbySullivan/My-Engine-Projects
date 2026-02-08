681b456a34ac0f4798cce5f795e32a865e27670d
(C35) Colby Sullivan <cwsullivan@smu.edu>
2026-02-07 19:52:46 -0600

    More efficient euler angles
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 449815]

--- Engine/Code/Engine/Math/EulerAngles.cpp
+++ Engine/Code/Engine/Math/EulerAngles.cpp
@@ -22,6 +22,7 @@ Vec3 EulerAngles::GetForwardDir_IFwd_JLeft_KUp() const
 //------------------------------------------------------------------------------
 void EulerAngles::GetAsVectors_IFwd_JLeft_KUp(Vec3& out_forwardIBasis, Vec3& out_leftJBasis, Vec3& out_upKBasis) const
 {
+#if defined(USE_MAT44_FOR_EULER_ANGLES)
 	Mat44 eulerMat;
 	eulerMat.AppendZRotation(m_yawDegrees);
 	eulerMat.AppendYRotation(m_pitchDegrees);
@@ -29,16 +30,42 @@ void EulerAngles::GetAsVectors_IFwd_JLeft_KUp(Vec3& out_forwardIBasis, Vec3& out
 	out_forwardIBasis = eulerMat.GetIBasis3D();
 	out_leftJBasis = eulerMat.GetJBasis3D();
 	out_upKBasis = eulerMat.GetKBasis3D();
+#else
+	float cosY = CosDegrees( m_yawDegrees );
+	float sinY = SinDegrees( m_yawDegrees );
+	float cosP = CosDegrees( m_pitchDegrees );
+	float sinP = SinDegrees( m_pitchDegrees );
+	float cosR = CosDegrees( m_rollDegrees );
+	float sinR = SinDegrees( m_rollDegrees );
+
+	out_forwardIBasis = Vec3( cosY * cosP, sinY * cosP, -sinP );
+	out_leftJBasis = Vec3( ( cosY * sinP * sinR ) - ( sinY * cosR ), ( sinY * sinP * sinR ) + ( cosY * cosR ), cosP * sinR );
+	out_upKBasis = Vec3( ( cosY * sinP * cosR ) + ( sinY * sinR ), ( sinY * sinP * cosR ) - ( cosY * sinR ), cosP * cosR );
+#endif
 }
 
 //------------------------------------------------------------------------------
 Mat44 EulerAngles::GetAsMatrix_IFwd_JLeft_KUp() const
 {
+#if defined(USE_MAT44_FOR_EULER_ANGLES)
 	Mat44 eulerMat;
 	eulerMat.AppendZRotation(m_yawDegrees);
 	eulerMat.AppendYRotation(m_pitchDegrees);
 	eulerMat.AppendXRotation(m_rollDegrees);
 	return eulerMat;
+#else
+	float cosY = CosDegrees( m_yawDegrees );
+	float sinY = SinDegrees( m_yawDegrees );
+	float cosP = CosDegrees( m_pitchDegrees );
+	float sinP = SinDegrees( m_pitchDegrees );
+	float cosR = CosDegrees( m_rollDegrees );
+	float sinR = SinDegrees( m_rollDegrees );
+	Mat44 eulerMat;
+	eulerMat.SetIJK3D(  Vec3( cosY * cosP, sinY * cosP, -sinP ),  
+						Vec3( ( cosY * sinP * sinR ) - ( sinY * cosR ), ( sinY * sinP * sinR ) + ( cosY * cosR ), cosP * sinR ), 
+						Vec3( ( cosY * sinP * cosR ) + ( sinY * sinR ), ( sinY * sinP * cosR ) - ( cosY * sinR ), cosP * cosR ) );
+	return eulerMat;
+#endif
 }
 
 //------------------------------------------------------------------------------
