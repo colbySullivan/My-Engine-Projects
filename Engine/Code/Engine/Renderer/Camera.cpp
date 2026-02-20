f7492644d76f941d04b2413808723a3bb2781839
(C35) Colby Sullivan <cwsullivan@smu.edu>
2026-02-19 20:25:27 -0600

    Protogame camera setup // Notice: Does not build
    [git-p4: depot-paths = "//depot/C35/Students/cwsullivan/SD/": change = 451290]

--- Engine/Code/Engine/Renderer/Camera.cpp
+++ Engine/Code/Engine/Renderer/Camera.cpp
@@ -1,17 +1,62 @@
 #include "Camera.hpp"
 
-void Camera::SetOrthoView(Vec2 const& bottomLeft, Vec2 const& topRight)
+//-----------------------------------------------------------------------------------------------
+void Camera::SetOrthographicView( Vec2 const& bottomLeft, Vec2 const& topRight, float near /*= 0.0f*/, float far /*= 1.0f */ )
 {
-	m_orthoBottomLeft = bottomLeft;
-	m_orthoTopRight = topRight;
+	m_orthographicBottomLeft = bottomLeft;
+	m_orthographicTopRight = topRight;
+	m_orthographicNear = near;
+	m_orthographicFar = far;
 }
 
-Vec2 Camera::GetOrthoBottomLeft() const
+//-----------------------------------------------------------------------------------------------
+void Camera::SetPerspectiveView( float aspect, float fov, float near, float far )
 {
-	return Vec2(m_orthoBottomLeft.x, m_orthoBottomLeft.y );
+	m_perspectiveAspect = aspect;
+	m_perspectiveFOV = fov;
+	m_perspectiveNear = near;
+	m_perspectiveFar = far;
 }
 
-Vec2 Camera::GetOrthoTopRight() const
+//-----------------------------------------------------------------------------------------------
+Mat44 Camera::GetCameraToWorldTransform() const
 {
-	return Vec2(m_orthoTopRight.x, m_orthoTopRight.y);
+	Mat44 cameraToWorld;
+	Mat44 orientationRotation = m_orientation.GetAsMatrix_IFwd_JLeft_KUp();
+	cameraToWorld.AppendTranslation3D(m_position);
+	cameraToWorld.Append(orientationRotation);
+
+	return cameraToWorld;
+}
+
+//-----------------------------------------------------------------------------------------------
+Mat44 Camera::GetWorldToCameraTransform() const
+{
+	return GetCameraToWorldTransform().GetOrthonormalInverse();
+}
+
+//-----------------------------------------------------------------------------------------------
+Mat44 Camera::GetOrthographicMatrix() const
+{
+	return Mat44::MakeOrthoProjection( m_orthographicBottomLeft.x, m_orthographicTopRight.x, m_orthographicBottomLeft.y, m_orthographicTopRight.y, m_orthographicNear, m_orthographicFar );
+}
+
+//-----------------------------------------------------------------------------------------------
+Mat44 Camera::GetPerspectiveMatrix() const
+{
+	return Mat44::MakePerspectiveProjection( m_perspectiveFOV, m_perspectiveAspect, m_perspectiveNear, m_perspectiveFar );
+}
+
+//-----------------------------------------------------------------------------------------------
+Mat44 Camera::GetProjectionMatrix() const
+{
+	if ( m_mode == eMode_Orthographic )
+	{
+		return GetOrthographicMatrix();
+	}
+	if ( m_mode == eMode_Perspective )
+	{
+		return GetPerspectiveMatrix();
+	}
+	return GetPerspectiveMatrix();
 }
--- Engine/Code/Engine/Renderer/Camera.hpp
+++ Engine/Code/Engine/Renderer/Camera.hpp
@@ -1,13 +1,65 @@
 #pragma once
 #include <Engine/Math/Vec2.hpp>
+#include <Engine/Math/Vec3.hpp>
+#include <Engine/Math/Mat44.hpp>
+#include <Engine/Math/EulerAngles.hpp>
+
 class Camera
 {
-	public:
-		void SetOrthoView(Vec2 const& bottomLeft, Vec2 const& topRight);
-		Vec2 GetOrthoBottomLeft() const;
-		Vec2 GetOrthoTopRight() const;
+public:
+	enum Mode
+	{
+		eMode_Orthographic,
+		eMode_Perspective,
 
-	private:
-		Vec2 m_orthoBottomLeft;
-		Vec2 m_orthoTopRight;
+		eMode_Count
+	};
+
+	void SetOrthographicView( Vec2 const& bottomLeft, Vec2 const& topRight, float near = 0.0f, float far = 1.0f );
+	void SetPerspectiveView( float aspect, float fov, float near, float far );
+
+	void SetPositionAndOrientation( const Vec3& position, const EulerAngles& orientation );
+	void SetPosition( const Vec3& position );
+	Vec3 GetPosition() const;
+	void SetOrientation( const EulerAngles& orientation );
+	EulerAngles GetOrientation() const;
+
+	Mat44 GetCameraToWorldTransform() const;
+	Mat44 GetWorldToCameraTransform() const;
+
+	void SetCameraToRenderTransform( const Mat44& m );
+	Mat44 GetCameraToRenderTransform() const;
+
+	Mat44 GetRenderToClipTransform() const;
+
+	Vec2 GetOrthographicBottomLeft() const;
+	Vec2 GetOrthographicTopRight() const;
+	void Translate2D( Vec2 const& translation );
+
+	Mat44 GetOrthographicMatrix() const;
+	Mat44 GetPerspectiveMatrix() const;
+	Mat44 GetProjectionMatrix() const;
+
+	// Legacy support
+	void SetOrthoView( Vec2 const& bottomLeft, Vec2 const& topRight ) { SetOrthographicView( bottomLeft, topRight ); }
+	Vec2 GetOrthoBottomLeft() const { return GetOrthographicBottomLeft(); }
+	Vec2 GetOrthoTopRight() const { return GetOrthographicTopRight(); }
+
+protected:
+	Mode m_mode = eMode_Orthographic;
+
+	Vec3 m_position;
+	EulerAngles m_orientation;
+
+	Vec2 m_orthographicBottomLeft;
+	Vec2 m_orthographicTopRight;
+	float m_orthographicNear;
+	float m_orthographicFar;
+
+	float m_perspectiveAspect;
+	float m_perspectiveFOV;
+	float m_perspectiveNear;
+	float m_perspectiveFar;
+
+	Mat44 m_cameraToRenderTransform;
 };
